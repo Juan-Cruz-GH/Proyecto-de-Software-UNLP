3a0ffce7b8ee41aade05523dea50a73294cc2cdb
Juan Cruz Cassera Botta <juancasserab@outlook.com.ar>
2022-09-24 18:28:10 -0300

    modificados

--- admin/src/core/board/issue.py
+++ admin/src/core/board/issue.py
@@ -2,11 +2,7 @@ import psycopg2
 import psycopg2.extras
 class Issue:
     def getAll(self):
-        conn = psycopg2.connect(
-        host="localhost",
-        database="Test",
-        user="postgres",
-        password="proyecto")
+        conn = psycopg2.connect(host="localhost", database="proyecto_db", user="postgres", password="proyecto")
         cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
         cur.execute('select * from issues')
         issues = cur.fetchall()
