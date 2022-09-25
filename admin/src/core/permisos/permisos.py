ba4a4ac450c00642405c709aa130776eebb04fe4
Leiva Hernán <hernanleiva1998@gmail.com>
2022-09-25 14:31:37 -0300

    Modelos permiso y rol con sus relaciones

--- admin/src/core/permisos/permisos.py
+++ admin/src/core/permisos/permisos.py
@@ -1,7 +1,18 @@
 from src.core.db import db
+#Tabla N a N de Permiso con Rol
+Permiso_Rol=db.Table(
+    "Permiso_Rol",
+    db.column("id_permiso",db.Integer,db.ForeignKey("Permisos.id"), nullable=False),
+    db.column("id_rol", db.Integer,db.ForeignKey("Roles.id"), nullable=False),
+)
 
 class Permiso(db.model):
     __tablename__ = "Permisos"
+
+    #columnas
     id=db.column(db.Integer, primary_key=True, nullable=False, unique=True)
     nombre= db.column(db.String(100), nullable=False)
+    
+    #relacion
+    roles=db.relationship("Rol", secondary=Permiso_Rol)
 
--- admin/src/core/roles/roles.py
+++ admin/src/core/roles/roles.py
@@ -1,7 +1,18 @@
 from src.core.db import db
 
+#Tabla N a N de Usuario con Rol
+Usuario_Rol=db.Table(
+    "Permiso_Rol",
+    db.column("id_usuario",db.Integer,db.ForeignKey("Usuario.id"), nullable=False),
+    db.column("id_rol", db.Integer,db.ForeignKey("Roles.id"), nullable=False),
+)
+
 class Rol(db.model):
     __tablename__="Roles"
-    id=db.Column(db.Integer, primary_key=True, nullable=False, unique=True)
-    nombre= db.Column(db.String(100), nullable=False)
-    
+
+    #Columnas
+    id=db.column(db.Integer, primary_key=True, nullable=False, unique=True)
+    nombre= db.column(db.String(100), nullable=False)
+
+    #Relacion
+    usuarios=db.relationship("Usuario", secondary=Usuario_Rol)
