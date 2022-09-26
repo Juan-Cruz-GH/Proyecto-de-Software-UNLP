ff8cecf91d9123238626ea6390aea16390b92e9e
Leiva Hernán <hernanleiva1998@gmail.com>
2022-09-26 17:44:06 -0300

    template inicial, blueprint y controller inicial

--- admin/src/web/__init__.py
+++ admin/src/web/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from src.web.helpers import handlers
 from src.web.controllers.issues import issue_blueprint
+from src.web.controllers.configuracion_sistema import configuracion_sistema
 from src.web.config import config
 from src.core.db import db, init_db
 
@@ -15,6 +16,7 @@ def create_app(env="development", static_folder="static"):
         return render_template("index.html", **kwargs)
         
     app.register_blueprint(issue_blueprint)
+    app.register_blueprint(configuracion_sistema)
 
     with app.app_context():
         init_db(app)
