42875de9e84b893a8d65e3ef334e6c90bbdb8d01
Leiva Hernán <hernanleiva1998@gmail.com>
2022-09-27 16:29:16 -0300

    modelo inicial de configuracion del sistema

--- admin/src/core/configuracion_sistema/configuracion_paginado.py
+++ admin/src/core/configuracion_sistema/configuracion_paginado.py
@@ -3,10 +3,10 @@ from src.core.db import db
 class Configuracion_paginado(db.Model):
     __tablename__="configuracion_paginado"
 
-    id=db.column(db.Integer, primary_key=True, nullable= False, unique=True)
+    id=db.Column(db.Integer, primary_key=True, nullable= False, unique=True)
     elementos_pagina=db.Column(db.Integer, nullable=False)
 
-    def __init__(self, elementos_pagina=none):
+    def __init__(self, elementos_pagina=None):
         self.elementos_pagina=elementos_pagina
 
     
