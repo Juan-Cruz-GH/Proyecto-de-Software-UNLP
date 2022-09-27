6c3d370e0c9b12175e21d7268a7b394b81377989
Leiva Hernán <hernanleiva1998@gmail.com>
2022-09-27 16:50:09 -0300

    corregir un par de erratas

--- admin/src/core/roles/roles.py
+++ admin/src/core/roles/roles.py
@@ -3,16 +3,16 @@ from src.core.db import db
 #Tabla N a N de Usuario con Rol
 Usuario_Rol=db.Table(
     "Permiso_Rol",
-    db.column("id_usuario",db.Integer,db.ForeignKey("Usuario.id"), nullable=False, primary_key=True),
-    db.column("id_rol", db.Integer,db.ForeignKey("Roles.id"), nullable=False, primary_key=True),
+    db.Column("id_usuario",db.Integer,db.ForeignKey("Usuario.id"), nullable=False, primary_key=True),
+    db.Column("id_rol", db.Integer,db.ForeignKey("Roles.id"), nullable=False, primary_key=True),
 )
 
 class Rol(db.model):
     __tablename__="Roles"
 
     #Columnas
-    id=db.column(db.Integer, primary_key=True, nullable=False, unique=True)
-    nombre= db.column(db.String(100), nullable=False)
+    id=db.Column(db.Integer, primary_key=True, nullable=False, unique=True)
+    nombre= db.Column(db.String(100), nullable=False)
 
     #Relacion
     usuarios=db.relationship("Usuario", secondary=Usuario_Rol)
