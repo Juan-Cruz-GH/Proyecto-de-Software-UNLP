b405f59530a9ed3a480c80928e83e8f1326c4dd8
Juan Cruz Cassera Botta <juancasserab@outlook.com.ar>
2022-09-29 19:49:01 -0300

    Operaciones CRUD en la BD

--- admin/src/core/disciplinas/__init__.py
+++ admin/src/core/disciplinas/__init__.py
@@ -1,5 +1,35 @@
 from src.core.disciplinas.disciplinas import Disciplina
+from src.core.db import db
 
 def listar_disciplinas(page):
-    return Disciplina.query.paginate(page, per_page=1)  # hardcodeado a 1 por ahora, luego consultará al modulo de cfg
+    '''Listado de las disciplinas según el paginado definido en el módulo de configuración.'''
+    return Disciplina.query.paginate(page, per_page=5)  # hardcodeado a 5 por ahora, luego consultará al modulo de cfg
 
+def agregar_disciplina(data):
+    '''Dar de alta una disciplina en la BD, chequeando que no exista primero.'''
+    if( Disciplina.query.filter_by(nombre = data["nombre"]).first() ):
+        return "Ya existe la disciplina."
+    else:
+        disciplina = Disciplina(**data)
+        db.session.add(disciplina)
+        db.session.commit()
+        return disciplina
+
+def eliminar_disciplina(id):
+    '''Dar de baja una disciplina en la BD.'''
+    disciplina = Disciplina.query.get(id)
+    if(disciplina is not None):
+        db.session.delete(disciplina)
+        db.session.commit()
+
+def modificar_disciplina(data):
+    '''Modificar datos de una disciplina en la BD.'''
+    disciplina = Disciplina.query.get(data["id"])
+    disciplina.nombre = data["nombre"]
+    disciplina.categoria = data["categoria"]
+    disciplina.instructores = data["instructores"]
+    disciplina.horarios = data["horarios"]
+    disciplina.costo = data["costo"]
+    disciplina.habilitada = data["habilitada"]
+    db.session.commit()
+    return disciplina
