df1b3b04c87c627397de268d16e7d038dcb4ba6d
Juan Cruz Cassera Botta <juancasserab@outlook.com.ar>
2022-09-29 19:25:54 -0300

    Cambios en disciplinas debido a lo discutido

--- admin/src/core/disciplinas/disciplinas.py
+++ admin/src/core/disciplinas/disciplinas.py
@@ -12,26 +12,20 @@ class Disciplina(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(50), nullable=False)
     categoria = db.Column(db.String(50), nullable=False)
-    nombre_instructor = db.Column(db.String(30), nullable=False)
-    apellido_instructor = db.Column(db.String(40), nullable=False)
-    dias = db.Column(db.String(100), nullable=False)
-    horarios = db.Column(db.String, nullable=False)
-    costo = db.Column(db.String, nullable=False)        # El formato de la moneda debe respetar el configurado en el modulo de cfg.
+    instructores = db.Column(db.String(200), nullable=False)
+    horarios = db.Column(db.String(150), nullable=False)
+    costo = db.Column(db.String, nullable=False)  # El formato de la moneda debe respetar el configurado en el modulo de cfg.
     habilitada = db.Column(db.Boolean, nullable=False)
     inserted_at = db.Column(db.DateTime, default=datetime.now)
-
     socios = db.relationship("Socio", secondary=Socio_Disciplina)
 
-    def __init__(self, nombre=None, categoria=None, nombre_instructor=None, apellido_instructor=None,
-    dias=None, horarios=None, costo=None, habilitada=None):
+    def __init__(self, nombre, categoria, instructores, horarios, costo, habilitada):
         self.nombre = nombre
         self.categoria = categoria
-        self.nombre_instructor = nombre_instructor
-        self.apellido_instructor = apellido_instructor
-        self.dias = dias
+        self.instructores = instructores
         self.horarios = horarios
         self.costo = costo
         self.habilitada = habilitada
-
+    
     def __repr__(self):
         return f"Disciplina (id={self.id!r}, nombre={self.nombre!r}, categoria={self.categoria!r})"
