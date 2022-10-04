dfd29484c4877d45396770ec49cd7dc17ec67a51
Manuel Trotta <nicolas.edlp98@gmail.com>
2022-10-04 16:23:36 -0300

    Solucion error de puerto para la conexion a la BD

--- admin/src/web/config.py
+++ admin/src/web/config.py
@@ -14,7 +14,7 @@ class ProductionConfig(Config):
     DB_PASS = environ.get("DB_PASS")
     DB_HOST = environ.get("DB_HOST")
     DB_NAME = environ.get("DB_NAME")
-    SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:5342/{DB_NAME}"
+    SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:5432/{DB_NAME}"
 
 
 class DevelopmentConfig(Config):
