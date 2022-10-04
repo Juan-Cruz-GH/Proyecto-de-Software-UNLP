329571ef530de0cfef44768d0c857e5e40578daa
Juan Cruz Cassera Botta <juancasserab@outlook.com.ar>
2022-10-04 17:32:17 -0300

    CRUD de disciplinas, falta validaciones

--- admin/src/core/disciplinas/__init__.py
+++ admin/src/core/disciplinas/__init__.py
@@ -5,6 +5,9 @@ def listar_disciplinas(page):
     '''Listado de las disciplinas según el paginado definido en el módulo de configuración.'''
     return Disciplina.query.paginate(page, per_page=5)  # hardcodeado a 5 por ahora, luego consultará al modulo de cfg
 
+def buscar_disciplina(id):
+    return Disciplina.query.get(id)
+
 def agregar_disciplina(data):
     '''Dar de alta una disciplina en la BD, chequeando que no exista primero.'''
     if( Disciplina.query.filter_by(nombre = data["nombre"]).first() is not None):
