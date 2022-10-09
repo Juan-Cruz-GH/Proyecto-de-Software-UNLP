c43d1110f0cca50e8363646dd4e7eda4a919e59e
Leiva Hernán <hernanleiva1998@gmail.com>
2022-10-09 16:59:17 -0300

    probando forms

--- admin/src/core/configuracion_sistema/configuracion_general.py
+++ admin/src/core/configuracion_sistema/configuracion_general.py
@@ -9,3 +9,15 @@ class Configuracion_general(db.Model):
     informacion_contacto=db.Column(db.String(500), nullable=False)
     cuota_base= db.Column(db.Float, nullable=False)
     porcentaje_recargo=db.Column(db.Float, nullable=False)
+
+
+    def __init__(self, activar_pagos, encabezado_recibidos, informacion_contacto, cuota_base,porcentaje_recargo) :
+        self.activar_pagos=activar_pagos
+        self.encabezado_recibos=encabezado_recibidos
+        self.informacion_contacto=informacion_contacto
+        self.cuota_base=cuota_base
+        self.porcentaje_recargo=porcentaje_recargo
+
+    def __repr__(self) :
+        return f"ConfigGeneral({self.encabezado_recibos} {self.informacion_contacto} {self.cuota_base} {self.porcentaje_recargo} activar pagos {self.activar_pagos} )"
+        
--- admin/src/web/controllers/configuracion_sistema.py
+++ admin/src/web/controllers/configuracion_sistema.py
@@ -1,5 +1,8 @@
+from src.core.configuracion_sistema.configuracion_paginado import Configuracion_paginado
 from flask import Blueprint, render_template
 from src.core import configuracion_sistema
+from src.core.db import db
+from flask import request
 
 
 configuracion_sistema_blueprint=Blueprint("configuracion_sistema",__name__, url_prefix="/configuracion_del_sistema")
@@ -7,5 +10,15 @@ configuracion_sistema_blueprint=Blueprint("configuracion_sistema",__name__, url_
 @configuracion_sistema_blueprint.get("/")
 def configuracion_index():
     kwargs = {"pageconfig": configuracion_sistema.getPaginado()}
-    kwargs2= {"generalconfig":configuracion_sistema.getConfiguracionGeneral()}
+    
+    #configuracion_sistema.getConfiguracionGeneral()
     return render_template("configuracion_sistema/configuracion_sistema.html", **kwargs)
+
+@configuracion_sistema_blueprint.route("/update", methods =["POST"])
+def configuracion_actualizar():
+    configuraciones ={
+            "elementos_pagina": request.form.get("elementos_pagina"),
+        }
+    config_paginado= Configuracion_paginado(**configuraciones)
+    db.session.add(config_paginado)
+    db.session.commit()
