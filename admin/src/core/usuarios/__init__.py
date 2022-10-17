542f15d9e95e3cc0591a9090843c12d261c6bcf9
Juan Cruz Cassera Botta <juancasserab@outlook.com.ar>
2022-10-16 22:45:35 -0300

    Limpieza general

--- admin/src/core/usuarios/__init__.py
+++ admin/src/core/usuarios/__init__.py
@@ -2,7 +2,6 @@ from src.core.db import db
 from src.core.usuarios.usuarios import Usuario
 import re
 
-
 def listar_usuarios(page, email=None, tipo=None):
     '''Esta funcion devuelve todos los usuarios de forma paginada segun la configuracion, y segun si se esta realizando una busqueda.'''
     if ((email is not None) and (tipo is not None)):
@@ -21,26 +20,21 @@ def listar_usuarios(page, email=None, tipo=None):
         usuarios = Usuario.query.paginate(page, per_page=1)
     return usuarios
 
-
 def agregar_usuario(data):
     '''Esta funcion se utiliza para dar de alta un usuario'''
     usuario = Usuario(**data)
     db.session.add(usuario)
     db.session.commit()
-
     return usuario
-
 
 def buscar_usuario(id):
     '''Esta funcion busca un usuario por su id'''
     usuario = Usuario.query.get(id)
     return usuario
 
-
 def find_user_by_mail_and_pass(email, password):
     '''esta funcion verifica que el usuario ingresado en login exista'''
     return Usuario.query.filter(Usuario.email == email, Usuario.password == password).first()
-
 
 def validar_inputs(email, password):
     '''Esta funcion valida que los inputs sean del tipo correcto. (falta comprobar password mediante hash)'''
@@ -54,10 +48,7 @@ def validar_inputs(email, password):
 
 def validar_estado(estado):
     '''Esta funcion valida el dato enviado al modificar el estado de un usuario'''
-    if(estado == "activo"):
-        return True
-    else:
-        return False
+    return (estado == "activo")
 
 def validar_datos_existentes(email, username, accion, id=None):
     '''Esta funcion valida que el dni o el email ingresados para dar de alta o modificar un usuario no existan.
@@ -76,14 +67,11 @@ def validar_datos_existentes(email, username, accion, id=None):
         email_existente = Usuario.query.filter_by(email=email).filter(Usuario.id != id).first()
         username_existente = Usuario.query.filter_by(username=username).filter(Usuario.id != id).first()
         if(email_existente is not None):
-            print('entro por email')
             return False, "El Email ya esta cargado en el sistema."
         elif(username_existente is not None):
-            print('entro por username')
             return False, "El Nombre de Usuario ya esta cargado en el sistema."
         else:
             return True, "Ambos son validos"
-
 
 def modificar_usuario(data):
     '''Esta funcion realiza la modificacion de los datos de un usuario'''
@@ -96,10 +84,8 @@ def modificar_usuario(data):
     db.session.commit()
     return usuario
 
-
 def eliminar_usuario(id):
     '''Esta funcion realiza la eliminacion de un usuario de la BD'''
     usuario = Usuario.query.get(id)
     db.session.delete(usuario)
     db.session.commit()
-
