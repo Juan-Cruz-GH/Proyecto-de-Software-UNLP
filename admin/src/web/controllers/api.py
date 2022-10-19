5baac6c7ea295a812153fdecb3982f970c22f9b4
Juan Cruz Cassera Botta <juancasserab@outlook.com.ar>
2022-10-19 18:20:28 -0300

    Update esqueleto apis y fix api disciplinas

--- admin/src/web/controllers/api.py
+++ admin/src/web/controllers/api.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, make_response
 from src.web.controllers import disciplinas
 from src.web.controllers import configuracion_sistema
 from src.web.controllers import usuarios
@@ -7,54 +7,51 @@ from src.decoradores.login import login_requerido
 api_blueprint = Blueprint("api", __name__, url_prefix="/api")
 
 
-@api_blueprint.route("/club/info", methods=["GET"])
-def obtener_info():
-    """Obtiene el json con la informacion de contacto y lo retorna"""
-    return configuracion_sistema.info_contacto_json()
-
-
-@api_blueprint.route("/club/disciplinas", methods=["GET"])          
+@api_blueprint.route("/club/disciplinas", methods=["GET"])  # 1er entrega
 def obtener_disciplinas():
     """Obtiene el json con todas las disciplinas y lo retorna"""
-    return disciplinas.disciplina_json()
+    respuesta = make_response(disciplinas.disciplina_json(), 200)
+    respuesta.headers["Content-Type"] = "application/json"
+    return respuesta
 
 
-@api_blueprint.route("/auth", methods=["POST"])
-def obtener_token():
-    """"""
+@api_blueprint.route("/me/disciplinas", methods=["GET"])  # 1er entrega
+def obtener_disciplinas_usuario(id):
+    """Obtiene el json con todas las disciplinas del usuario que se envia por parametro y lo retorna"""
     pass
 
 
-@api_blueprint.route("/me/profile", methods=["GET"])
-@login_requerido
-def obtener_info_usuario():
-    """Obtiene el json con todos los datos del usuario logueado en este momento y lo retorna"""
-    return usuarios.info_usuario_logueado()
-
-
-@api_blueprint.route("/me/disciplinas", methods=["GET"])
-@login_requerido
-def obtener_disciplinas_usuario():
-    """Obtiene el json con todas las disciplinas del usuario logueado en este momento y lo retorna"""
-    pass
-
-
-@api_blueprint.route("/me/payments", methods=["GET"])
-@login_requerido
+@api_blueprint.route("/me/payments", methods=["GET"])  # 1er entrega
 def obtener_pagos():
     """Obtiene la lista de pagos registrados del usuario logueado en este momento y lo retorna"""
     pass
 
 
-@api_blueprint.route("/me/payments", methods=["POST"])
-@login_requerido
+@api_blueprint.route("/me/payments", methods=["POST"])  # 1er entrega
 def registrar_pagos():
     """Registra un nuevo pago para el usuario logueado en este momento"""
     pass
 
 
-@api_blueprint.route("/me/license", methods=["GET"])
-@login_requerido
+@api_blueprint.route("/me/profile", methods=["GET"])  # 2da entrega
+def obtener_info_usuario(id):
+    """Obtiene el json con todos los datos del usuario que se envia por parametro y lo retorna"""
+    return usuarios.info_usuario_logueado()
+
+
+@api_blueprint.route("/auth", methods=["POST"])  # 2da entrega
+def obtener_token():
+    """"""
+    pass
+
+
+@api_blueprint.route("/me/license", methods=["GET"])  # 2da entrega
 def obtener_info_credencial():
     """Obtiene la información personal y el estado de credencial del usuario logueado en este momento y lo retorna"""
     pass
+
+
+@api_blueprint.route("/club/info", methods=["GET"])  # 2da entrega
+def obtener_info():
+    """Obtiene el json con la informacion de contacto y lo retorna"""
+    return configuracion_sistema.info_contacto_json()
