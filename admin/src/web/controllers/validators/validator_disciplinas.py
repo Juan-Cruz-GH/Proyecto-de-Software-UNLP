b70b9010eac47b6ee03387a1666b61d52fa32835
Juan Cruz Cassera Botta <juancasserab@outlook.com.ar>
2022-10-20 20:23:36 -0300

    Modularización de las validaciones en disciplinas

--- admin/src/web/controllers/validators/validator_disciplinas.py
+++ admin/src/web/controllers/validators/validator_disciplinas.py
@@ -3,6 +3,21 @@ import re
 
 def validar_inputs(data):
     """Chequea que los datos del formulario sean válidos"""
+    if datos_estan_vacios(data):
+        return False, "Todos los datos deben llenarse"
+    if not costo_es_numero(data["costo"]):
+        return False, "El costo debe ser un numero"
+    if costo_es_negativo(data["costo"]):
+        return False, "El costo no puede ser negativo"
+    if not nombre_es_valido(data["nombre"]):
+        return False, "El nombre de la disciplina no puede tener numeros"
+    if not horario_es_valido(data["horarios"]):
+        return False, 'El formato de los horarios es incorrecto, debe ser "Dia1 Dia2 (opcional) de X a Yhs"'       
+    return True, "Los datos son validos"
+
+
+def datos_estan_vacios(data):
+    """Si alguno de los datos está vacio retorna verdadero"""
     if (
         data["nombre"] == ""
         or data["categoria"] == ""
@@ -11,17 +26,20 @@ def validar_inputs(data):
         or data["costo"] == ""
         or data["habilitada"] == ""
     ):
-        return False, "Todos los datos deben llenarse"
-    elif not data["costo"].lstrip("-").isdigit():
-        return False, "El costo debe ser un numero"
-    elif int(data["costo"]) < 0:
-        return False, "El costo no puede ser negativo"
-    elif not (re.fullmatch(r"[A-Za-z ]{1,50}", data["nombre"])):
-        return False, "El nombre de la disciplina no puede tener numeros"
-    elif "de" not in data["horarios"]:
-        return (
-            False,
-            'El formato de los horarios es incorrecto, debe ser "Dia1 Dia2 (opcional) de X a Yhs"',
-        )
-    else:
-        return True, "Los datos son validos"
+        return True
+
+
+def costo_es_numero(costo):
+    return costo.lstrip("-").isdigit()
+
+
+def costo_es_negativo(costo):
+    return (int(costo) < 0)
+
+
+def nombre_es_valido(nombre):
+    return re.fullmatch(r"[A-Za-z ]{1,50}", nombre)
+
+
+def horario_es_valido(horarios):
+    return "de" in horarios
