59630d5d898aef1d84a2ec64a15118b19ab00c00
Manuel Trotta <nicolas.edlp98@gmail.com>
2022-10-24 15:41:54 -0300

    Se establecieron como unicos el email y el dni en socios

--- admin/src/core/socios/socios.py
+++ admin/src/core/socios/socios.py
@@ -1,3 +1,4 @@
+from enum import unique
 from src.core.db import db
 from datetime import datetime
 
@@ -7,10 +8,10 @@ class Socio(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String, nullable=False)
     apellido = db.Column(db.String, nullable=False)
-    email = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
     activo = db.Column(db.Boolean, nullable=False)
     tipo_documento = db.Column(db.String, nullable=False)
-    dni = db.Column(db.String, nullable=False)
+    dni = db.Column(db.String, unique=True, nullable=False)
     genero = db.Column(db.String, nullable=False)
     direccion = db.Column(db.String, nullable=False)
     telefono = db.Column(db.String, nullable=False)
