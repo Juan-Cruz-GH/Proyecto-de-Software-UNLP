39e5a2a064136f419320a663b52a3820dd8c91db
Juan Cruz Cassera Botta <juancasserab@outlook.com.ar>
2022-11-05 18:36:32 -0300

    Apis Se eliminó flask_cors ya que ya se estaba usando en el init de src/web, se separó la validación de los headers, se reorganizaron y nombraron mejor algunas apis y se mejoraron algunos docstrings.

--- admin/src/web/controllers/api.py
+++ admin/src/web/controllers/api.py
@@ -2,16 +2,13 @@ from flask import Blueprint, make_response, request
 
 from src.web.controllers import disciplinas
 from src.web.controllers import configuracion_sistema
-from src.web.controllers import usuarios
-from src.web.controllers import socios
 from src.web.controllers import pagos
-from flask_cors import cross_origin
+from src.web.controllers.validators import validator_api
 
 api_blueprint = Blueprint("api", __name__, url_prefix="/api")
 
 
-@cross_origin
-@api_blueprint.route("/club/disciplinas", methods=["GET"])  # 1er entrega
+@api_blueprint.route("/club/disciplinas", methods=["GET"])
 def obtener_disciplinas():
     """Obtiene el json con todas las disciplinas y lo retorna"""
     respuesta = make_response(disciplinas.disciplina_json(), 200)
@@ -19,56 +16,51 @@ def obtener_disciplinas():
     return respuesta
 
 
-@cross_origin
-@api_blueprint.route("/me/disciplinas", methods=["GET"])  # 1er entrega
-def obtener_disciplinas_usuario():
-    """Obtiene el json con todas las disciplinas del usuario que se envia por parametro y lo retorna"""
-    if socios.disciplinas_socio(request.headers.get("id")) is None:
-        return make_response({"Error": "El usuario no existe"}, 400)
-    respuesta = make_response(socios.disciplinas_socio(request.headers.get("id")), 200)
+@api_blueprint.route("/me/disciplinas", methods=["GET"])
+def obtener_disciplinas_socio():
+    """Valida el header id y devuelve la respuesta en formato json"""
+    id = request.headers.get("id")
+    mensaje, http_code = validator_api.validar_header_disciplinas_socio(id)
+    respuesta = make_response(mensaje, http_code)
     respuesta.headers["Content-Type"] = "application/json"
     return respuesta
 
 
-@cross_origin
-@api_blueprint.route("/me/payments", methods=["GET"])  # 1er entrega
+@api_blueprint.route("/me/license", methods=["GET"])
+def obtener_info_y_estado_socio():
+    """Obtiene la información personal y el estado de credencial del socio enviado en el request y lo retorna"""
+    id = request.headers.get("id")
+    mensaje, http_code = validator_api.validar_header_estado_socio(id)
+    respuesta = make_response(mensaje, http_code)
+    respuesta.headers["Content-Type"] = "application/json"
+    return respuesta
+
+
+@api_blueprint.route("/me/payments", methods=["GET"])
 def obtener_pagos():
     """Obtiene la lista de pagos registrados del usuario logueado en este momento y lo retorna"""
     return pagos.pagos_json()
 
 
-@cross_origin
-@api_blueprint.route("/me/payments", methods=["POST"])  # 1er entrega
+@api_blueprint.route("/me/payments", methods=["POST"])
 def registrar_pagos():
     """Registra un nuevo pago para el usuario logueado en este momento"""
-    json = request.get_json()
-    return pagos.pagar_json(json)
+    return pagos.pagar_json(request.get_json())
 
 
-@api_blueprint.route("/me/profile", methods=["GET"])  # 2da entrega
-def obtener_info_usuario(id):
-    """Obtiene el json con todos los datos del usuario que se envia por parametro y lo retorna"""
-    return usuarios.info_usuario(id)
+@api_blueprint.route("/club/info", methods=["GET"])
+def obtener_info_club():
+    """Obtiene el json con la informacion de contacto y lo retorna"""
+    return configuracion_sistema.info_contacto_json()
 
 
-@api_blueprint.route("/auth", methods=["POST"])  # 2da entrega
+@api_blueprint.route("/auth", methods=["POST"])
 def obtener_token():
     """"""
     pass
 
 
-@cross_origin
-@api_blueprint.route("/me/license", methods=["GET"])  # 2da entrega
-def obtener_info_credencial():
-    """Obtiene la información personal y el estado de credencial del socio enviado en el request y lo retorna"""
-    if not socios.existe_socio(request.headers.get("id")):
-        return make_response({"Error": "El socio no existe"}, 400)
-    respuesta = make_response(socios.json_estado_socio(request.headers.get("id")), 200)
-    respuesta.headers["Content-Type"] = "application/json"
-    return respuesta
-
-
-@api_blueprint.route("/club/info", methods=["GET"])  # 2da entrega
-def obtener_info():
-    """Obtiene el json con la informacion de contacto y lo retorna"""
-    return configuracion_sistema.info_contacto_json()
+@api_blueprint.route("/me/profile", methods=["GET"])
+def obtener_info_usuario():
+    """Obtiene el json con todos los datos del usuario que se envia por parametro y lo retorna"""
+    pass
