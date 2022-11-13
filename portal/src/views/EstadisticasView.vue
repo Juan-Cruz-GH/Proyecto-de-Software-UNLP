4ae4c52d3bc1782397f8f45051987def45b2adca
Manuel Trotta <nicolas.edlp98@gmail.com>
2022-11-13 12:26:08 -0300

    Se agrego la vista de estadisticas

---
+++ portal/src/views/EstadisticasView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Estadisticas from "../components/estadisticas/Estadisticas.vue";
+import Footer from "../components/footer.vue"
+</script>
+
+<template>
+  <main>
+    <Estadisticas />
+    <Footer />
+  </main>
+</template>
