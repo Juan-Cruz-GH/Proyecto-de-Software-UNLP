28bc4eb4bb298462206502daa910bbf7a81a069d
Juan Cruz Cassera Botta <juancasserab@gmail.com>
2022-11-15 15:30:33 -0300

    Variables globales para url de apis

--- portal/src/main.js
+++ portal/src/main.js
@@ -8,6 +8,14 @@ import router from "./router";
 
 const app = createApp(App);
 
+const headerLocal = "http://127.0.0.1:5000/api";
+const headerRemoto = "https://admin-grupo23.proyecto2022.linti.unlp.edu.ar/api";
+
+app.provide("URL_API_DISCIPLINAS", headerLocal + "/club/disciplinas");
+app.provide("URL_API_LICENCIA", headerLocal + "/me/license");
+app.provide("URL_API_CLUB", headerLocal + "/club/info");
+// cambiar headerLocal por headerRemoto en los provides antes de push a main
+
 app.use(createPinia());
 app.use(router);
 
