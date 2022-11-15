5abee773823902871789bf11435fae8fccc35ab5
Manuel Trotta <nicolas.edlp98@gmail.com>
2022-11-15 17:01:09 -0300

    error CORS

--- admin/src/web/__init__.py
+++ admin/src/web/__init__.py
@@ -21,7 +21,7 @@ def create_app(env="development", static_folder="static"):
     app = Flask(__name__, static_folder=static_folder)
     app.config.from_object(config[env])
     csrf = CSRFProtect(app)
-    CORS(app)
+    #CORS(app)
     csrf.exempt(api_blueprint)
 
     @app.get("/")
