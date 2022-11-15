d3c0d7c8b4237b5c582f803494d5cd866c074250
Nicolas Pierini <nicolasepierini@gmail.com>
2022-11-15 02:05:47 -0300

    LoginPublico

--- portal/src/views/LoginView.vue
+++ portal/src/views/LoginView.vue
@@ -1,11 +1,107 @@
-<script setup>
-import Auth from "../components/auth/Auth.vue";
-import Footer from "../components/footer.vue"
-</script>
-
 <template>
-    <main>
-        <Auth />
-        <Footer />
-    </main>
+  <header>
+    <div id="navbar">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+          <RouterLink class="navbar-brand" to="/">Club Villa Elisa</RouterLink>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        </div>
+      </nav>
+    </div>
+  </header>
+
+  <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+    <div class="card border-0 shadow rounded-3 my-5">
+      <div class="card-body p-4 p-sm-5">
+        <h5 class="card-title text-center mb-5 fw-light fs-5">Log In</h5>
+        <form action class="form" @submit.prevent="login">
+          <div class="mb-3">
+            <label class="form-label" for="#socio.email">Email</label>
+            <input
+              class="form-control"
+              type="email"
+              name="email"
+              placeholder="Correo"
+              v-model="socio.email"
+              id="email"
+              required
+            />
+          </div>
+          <div class="mb-3">
+            <label class="form-label" for="#socio.password">Password</label>
+            <input
+              class="form-control"
+              type="password"
+              name="password"
+              placeholder="Clave"
+              v-model="socio.password"
+              id="password"
+              required
+            />
+          </div>
+          <input
+            class="btn btn-primary btn-login text-uppercase fw-bold"
+            type="submit"
+            value="Login"
+          />
+        </form>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+  import { mapActions, mapGetters } from 'vuex'
+
+  export default {
+    name: 'auth',
+    data: () => ({
+      socio: {
+        email: null,
+        password: null
+      }
+    }),
+
+    computed: {
+      mapGetters({
+        authSocio: 'auth/socio',
+        isLoggedIn: 'auth/isLoggedIn'
+      }),
+    },
+
+    methods: {
+      mapActions('auth',['loginSocio','logoutSocio']),
+
+      async login() {
+        await this.loginSocio(this.socio)
+        this.socio = {
+          email = null,
+          password = null
+        }
+        
+        if (this.isLoggedIn) {
+          this.$router.push('/login')
+        }
+      },
+
+      async logout() {
+        await this.logoutSocio()
+        this.socio = {
+          email = null,
+          password = null
+        }
+        this.$router.push('/');
+      },
+    }
+  },
+</script>
