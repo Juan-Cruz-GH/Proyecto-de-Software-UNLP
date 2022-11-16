446319a7ee7fad4031d8276e30838d2ce0028fdb
Juan Cruz Cassera Botta <juancasserab@gmail.com>
2022-11-16 16:02:22 -0300

    Views de cuotas

---
+++ portal/src/views/CuotasImpagasView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import ListadoCuotasImpagas from "../components/pagos/ListadoCuotasImpagas.vue";
+import Footer from "../components/footer.vue"
+</script>
+
+<template>
+    <main>
+        <ListadoCuotasImpagas />
+        <Footer />
+    </main>
+</template>
---
+++ portal/src/views/CuotasPagasView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import ListadoCuotasPagas from "../components/pagos/ListadoCuotasPagas.vue";
+import Footer from "../components/footer.vue"
+</script>
+
+<template>
+    <main>
+        <ListadoCuotasPagas />
+        <Footer />
+    </main>
+</template>
