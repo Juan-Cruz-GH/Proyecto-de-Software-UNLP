60921b716ace836819b194680580411b2959394d
Juan Cruz Cassera Botta <juancasserab@gmail.com>
2022-11-17 18:20:30 -0300

    Se borraron 3 rutas y se agregó una unificada

--- portal/src/router/index.js
+++ portal/src/router/index.js
@@ -2,8 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import DisciplinasView from "../views/DisciplinasView.vue";
 import EstadisticasView from "../views/EstadisticasView.vue";
-import CuotasImpagasView from "../views/CuotasImpagasView.vue";
-import CuotasPagasView from "../views/CuotasPagasView.vue";
+import PagosView from "../views/PagosView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,14 +11,6 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView,
-    },
-    {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
     },
     {
       path: "/disciplinas",
@@ -32,15 +23,10 @@ const router = createRouter({
       component: EstadisticasView,
     },
     {
-      path: "/cuotasPagas",
-      name: "cuotasPagas",
-      component: CuotasPagasView,
-    },
-    {
-      path: "/cuotasImpagas",
-      name: "cuotasImpagas",
-      component: CuotasImpagasView,
-    },
+      path: "/pagos",
+      name: "pagos",
+      component: PagosView,
+    }
   ],
 });
 
