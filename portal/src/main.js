7a643343f480e599006e3a7d277e727c08860f77
Juan Cruz Cassera Botta <juancasserab@gmail.com>
2022-11-17 18:53:27 -0300

    Comentario en main

--- portal/src/main.js
+++ portal/src/main.js
@@ -8,15 +8,15 @@ import router from "./router";
 
 const app = createApp(App);
 
-const header = "http://127.0.0.1:5000";
-// https://admin-grupo23.proyecto2022.linti.unlp.edu.ar     header remoto
-// http://127.0.0.1:5000                                    header local
+const header = "https://admin-grupo23.proyecto2022.linti.unlp.edu.ar";
+// https://admin-grupo23.proyecto2022.linti.unlp.edu.ar     cambiar header a este antes de cada push/merge a main
+// http://127.0.0.1:5000                                    cambiar header a este para correr la app localmente
 
 app.provide("URL_API_DISCIPLINAS", header + "/api/club/disciplinas");
 app.provide("URL_API_LICENCIA", header + "/api/me/license");
 app.provide("URL_API_CLUB", header + "/api/club/info");
-app.provide("URL_SOCIOS_GENERO", header + "api/club/socios-genero")
-app.provide("URL_SOCIOS_AÑO", header + "api/club/socios-años")
+app.provide("URL_API_SOCIOS_GENERO", header + "/api/club/socios-genero")
+app.provide("URL_API_SOCIOS_AÑO", header + "/api/club/socios-años")
 app.provide("URL_API_PAYMENTS", header + "/api/me/payments") // GET en ListadoCuotas, POST para pagar.
 
 app.use(createPinia());
