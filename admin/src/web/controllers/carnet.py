e1e671a0cdd5fecd907fc8b22bc85f89ac928c89
Leiva Hernán <hernanleiva1998@gmail.com>
2022-11-22 15:46:32 -0300

    cargar imagen permisos y login required

--- admin/src/web/controllers/carnet.py
+++ admin/src/web/controllers/carnet.py
@@ -1,4 +1,5 @@
 import io
+
 from flask import (
     Blueprint,
     render_template,
@@ -15,9 +16,13 @@ from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 from wtforms import SubmitField
 from flask_uploads import UploadSet, IMAGES
+from pathlib import Path, PurePosixPath
 
 from src.core.socios import buscar_socio, save_photo, get_photo_socio
 from src.web.exportaciones import carnet_PDF
+from src.web.helpers.permission import has_permission
+from src.decoradores.login import login_requerido
+
 
 carnet_blueprint = Blueprint("carnet", __name__, url_prefix="/carnet")
 photos = UploadSet("photos", IMAGES)
@@ -34,12 +39,18 @@ class UploadForm(FlaskForm):
 
 
 @carnet_blueprint.route("public/uploads/<filename>")
+@login_requerido
 def get_file(filename):
+    if not (has_permission(session["user"], "carnet_photo")):
+        return abort(403)
     return send_from_directory("public/uploads", filename)
 
 
 @carnet_blueprint.route("/upload_image/<id>", methods=["GET", "POST"])
+@login_requerido
 def upload_image(id):
+    if not (has_permission(session["user"], "carnet_upload")):
+        return abort(403)
     socio = buscar_socio(id)
     form = UploadForm()
     if form.validate_on_submit():
@@ -55,21 +66,31 @@ def upload_image(id):
 
 
 @carnet_blueprint.route("/<id>")
+@login_requerido
 def view_license(id):
+    if not (has_permission(session["user"], "carnet_license")):
+        return abort(403)
     kwargs = {
         "url": request.url,
         "socio": buscar_socio(id),
         "photo": get_photo_socio(id),
     }
-
+    print(image_exists(kwargs["photo"]))
+    if not image_exists(kwargs["photo"]):
+        kwargs["photo"] = get_default_photo_path()
     return render_template("carnet/carnet_template.html", **kwargs)
 
 
 @carnet_blueprint.route("/<id>/carnet.png")
 def view_license_only(id):
-    kwargs = {"url": request.url, "socio": buscar_socio(id)}
-    strIO = io.BytesIO()
-    strIO.readlines(render_template("carnet/carnet_only.html", **kwargs))
-    strIO.seek(0)
-    return send_file(strIO, download_name="carnet.pdf", as_attachment=True)
-    # return render_template("carnet/carnet_only.html", **kwargs)
+    return render_template("carnet/carnet_only.html")
+
+
+def image_exists(path):
+    path = str(Path(__file__).parent.parent.parent.parent) + path
+    print(path)
+    return Path(path).exists()
+
+
+def get_default_photo_path():
+    return "/public/uploads/default_photo.jpg"
