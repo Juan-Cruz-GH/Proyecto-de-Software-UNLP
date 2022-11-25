d9ab57779ebb3484ed24d83e70a634bf5b8f9697
Nicolas Pierini <nicolasepierini@gmail.com>
2022-11-25 16:06:01 -0300

    arreglos

--- portal/src/components/carnet/carnet.vue
+++ portal/src/components/carnet/carnet.vue
@@ -16,10 +16,10 @@
         <div class="col-md-3">
             <img src="..." class="card-img" alt="...">
         </div>
-    </div>
     <p v-if="socio">
         {{ socio.profile.email }}
     </p>
+    </div>
 </template>
   
 <script>
