efb65e3a6e014ef8ec9c74d4646b880c6ed45043
Leiva Hernán <hernanleiva1998@gmail.com>
2022-11-27 15:49:33 -0300

    quitar cors

--- portal/src/api.js
+++ portal/src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const apiService = axios.create({
   // http://localhost:5000   -> baseURL para correr localmente
   // https://admin-grupo23.proyecto2022.linti.unlp.edu.ar    -> baseURL para correr en produccion
-  baseURL: "http://localhost:5000",
+  baseURL: "https://admin-grupo23.proyecto2022.linti.unlp.edu.ar",
   withCredentials: true,
   xsrfCookieName: "csrf_access_token",
 });
