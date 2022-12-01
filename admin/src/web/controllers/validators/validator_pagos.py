783f560177541615b06651cf482af257867f91b5
Juan Cruz Cassera Botta <juancasserab@gmail.com>
2022-12-01 19:20:34 -0300

    Fix error

--- admin/src/web/controllers/validators/validator_pagos.py
+++ admin/src/web/controllers/validators/validator_pagos.py
@@ -2,14 +2,14 @@ def validar_inputs(json):
     # El json en sí siempre va a ser enviado, por cómo está definido
     # el frontend y por tener el jwt_required en la api.
     if datos_estan_vacios(json):
-        return False, "El mes y el monto deben estar presentes"
+        return False  # El mes y el monto deben estar presentes
     if not es_numero(json["month"]):
-        return False, "El mes no es un número"
+        return False  # El mes no es un número
     if not es_numero(json["amount"]):
-        return False, "El monto no es un número"
+        return False  # El monto no es un número
     if not es_mes(json["month"]):
-        return False, "El mes no es un mes"
-    return True, "Datos válidos"
+        return False  # El mes no es un mes
+    return True  # Datos válidos
 
 
 def datos_estan_vacios(json):
