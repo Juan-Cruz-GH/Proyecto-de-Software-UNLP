7945e362be911b1852fbc6be5d10d3f4efa51ac9
Juan Cruz Cassera Botta <juancasserab@gmail.com>
2022-12-03 22:13:55 -0300

    Validator innecesario

--- admin/src/web/helpers/permission.py
+++ admin/src/web/helpers/permission.py
@@ -1,6 +1,12 @@
-from src.web.controllers.validators import validator_permisos
+from src.core import usuarios, permisos
 
 
 def has_permission(user, permission):
-    """Manda a validar al modelo si un usuario tiene los permisos necesarios para ingresar a una vista"""
-    return validator_permisos.has_permission(user, permission)
+    """Este metodo valida si el usuario posee o no el permiso enviado por parametro"""
+    usuario = usuarios.buscar_usuario_email(user)
+    roles = usuario.roles
+    permiso = permisos.buscar_permiso(permission)
+    for rol in roles:
+        if permiso in rol.permisos:
+            return True
+    return False
