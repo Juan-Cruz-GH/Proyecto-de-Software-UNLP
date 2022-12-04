29d8e319e6109f7807c733972b5152ad2ddd9b7b
Juan Cruz Cassera Botta <juancasserab@gmail.com>
2022-12-03 23:08:52 -0300

    Documentación color aleatorio

--- admin/src/core/colores_aleatorios.py
+++ admin/src/core/colores_aleatorios.py
@@ -1,8 +1,11 @@
 import random
 
+
 def generar_color():
+    """Genera un código HEX aleatorio que representa un color aleatorio"""
     color = "#"
-    hexadecimal = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"]
+    nums = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    letras = ["A", "B", "C", "D", "E", "F"]
     for i in range(6):
-        color += random.choice(hexadecimal)
+        color += random.choice(nums + letras)
     return color
