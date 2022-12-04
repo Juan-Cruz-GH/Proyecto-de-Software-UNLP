483d686e8f995307b9592b6f41f1def4f54c1921
Juan Cruz Cassera Botta <juancasserab@gmail.com>
2022-12-03 22:21:50 -0300

    Reuso de código

--- admin/src/web/controllers/validators/validator_pagos.py
+++ admin/src/web/controllers/validators/validator_pagos.py
@@ -1,10 +1,14 @@
-from src.web.controllers.validators.common_validators import is_integer
+from src.web.controllers.validators.common_validators import (
+    is_integer,
+    dict_values_are_none,
+    dict_values_are_empty,
+)
 
 
 def validar_inputs(json):
     # El json en sí siempre va a ser enviado, por cómo está definido
     # el frontend y por tener el jwt_required en la api.
-    if datos_estan_vacios(json):
+    if dict_values_are_none(json) or dict_values_are_empty(json):
         return False  # El mes y el monto deben estar presentes
     if not is_integer(json["month"]):
         return False  # El mes no es un número
@@ -13,12 +17,6 @@ def validar_inputs(json):
     if not es_mes(json["month"]):
         return False  # El mes no es un mes
     return True  # Datos válidos
-
-
-def datos_estan_vacios(json):
-    """Si alguno de los datos está vacio retorna verdadero"""
-    if json["month"] == "" or json["amount"] == "":
-        return True
 
 
 def es_mes(mes):
