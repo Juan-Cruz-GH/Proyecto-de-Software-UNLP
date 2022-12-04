1ee990e155f090136667f802f2ad95f0d2e1b59c
Juan Cruz Cassera Botta <juancasserab@gmail.com>
2022-12-03 23:23:39 -0300

    Cambio nombre de método

--- admin/src/web/controllers/socios.py
+++ admin/src/web/controllers/socios.py
@@ -219,7 +219,7 @@ def inscripcion_socio(id):
         return abort(403)
     kwargs = {
         "id_socio": id,
-        "disciplinas": disciplinas.todas_las_disciplinas(),
+        "disciplinas": disciplinas.nombres_todas_las_disciplinas(),
         "categorias": disciplinas.categorias_de_cada_disciplina(),
         "usuario": usuarios.buscar_usuario_email(session["user"]),
     }
